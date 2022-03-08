19897a062a77c7eaf993b0c061ee0f2c5f5d69aa
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-03-08 20:47:03 +0530

    1st commit

---
+++ Test.js
@@ -0,0 +1,13 @@
+xit('Launch URLS', function () {
+    browser.url('https://the-internet.herokuapp.com/login')
+});
+xit('refresh', function () {
+    browser.refresh()
+})
+xit('u', function () {
+    //var t =
+    browser.getTitle().then(function (tt) {
+        console.log('Title : ', tt);
+        assert.equal(tt, 'The Internet');
+    });
+});
---
+++ conf.js
@@ -0,0 +1,19 @@
+exports.config = {
+  capabilities: [{
+    'browserName': 'chrome'
+  }],
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  //specs: ['example_spec.js'],
+  specs: [
+    './test/specs/Main.js'
+],
+
+  jasmineOpts: {
+    defaultTimeoutInterval: 30000
+  }
+};
