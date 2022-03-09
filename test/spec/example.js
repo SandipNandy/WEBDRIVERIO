372a22de610f11cb37cb978b247d4b6bc20e67ec
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-03-09 10:38:44 +0530

    1st commit
    file is uploaded

---
+++ test/spec/example.js
@@ -0,0 +1,20 @@
+//const protractor=require('protractor');
+//const driver1=browser;
+const assert = require('assert');
+import LoginPage from '../pageobject/Locators.page';
+describe('First Test', function () {
+
+    it('Main Scripts',async function () {
+        await LoginPage.open();
+        await LoginPage.Title().then(function (tt) {
+             console.log('Title : ', tt);
+             assert.equal(tt, 'The Internet');
+         });
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+
+    });
+
+    
+
+
+});
