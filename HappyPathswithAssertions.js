5dea87d248680cb51bc2276a651f094963e1de54
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-04-26 10:56:28 +0530

    1st commit

---
+++ HappyPathswithAssertions.js
@@ -0,0 +1,16 @@
+describe('Assertions in Web DriverIO', () => {
+
+    it('First Drop down', async () => {
+        await browser.url('https://rahulshettyacademy.com/loginpagePractise/');
+        await browser.maximizeWindow();
+        await $("[id='username']").setValue('rahulshettyacademy');
+        await $("[id='password']").setValue("learning");
+        await (await $("[id='signInBtn']")).click();
+        await browser.pause(2000);
+        await (await $("[class='list-group-item']")).waitForExist({ timeout: 30000 });
+        expect(browser).toHaveUrlContaining("shop");
+        expect(browser).toHaveTitle('ProtoCommerce');
+
+
+    });
+});
