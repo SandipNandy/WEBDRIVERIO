c705cc2db6468804c588cf59d1130ca5625964ea
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-05-07 11:01:27 +0530

    1st commit

---
+++ test/spec/RadioButtonHandles.js
@@ -0,0 +1,17 @@
+describe('Radio Buttons Drop Down : ', () => {
+
+    it('DEMO PAGE.', async () => {
+        await browser.url('https://rahulshettyacademy.com/loginpagePractise/');
+        await browser.maximizeWindow();
+        await $("[id='username']").setValue('rahulshettyacademy');
+        await $("[id='password']").setValue("learning");
+        const radioButton= await $$(".radiotextsty");
+        let UserDropDown=radioButton[1];
+        await UserDropDown.$("span").click();
+        await browser.pause(4000);
+        //await (await $("[id='signInBtn']")).click();
+        
+
+
+    });
+});
