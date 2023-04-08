3d9a8da4e5124376488310bb53aca45fe3f44569
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-04-08 17:46:01 +0530

    Update E2ET1.js

--- EndtoEndTest/E2ET1.js
+++ EndtoEndTest/E2ET1.js
@@ -1,6 +1,7 @@
 describe('END TO END :-', () => {
     it('End to End Test Practice : ', async() => {
         const products=['iphone X','Samsung Note 8','Blackberry']
+        //https://rahulshettyacademy.com/loginpagePractise/
        await browser.url('https://rahulshettyacademy.com/loginpagePractise/');
        await $("#username").setValue("rahulshettyacademy");
         await $('#password').setValue("learning");
