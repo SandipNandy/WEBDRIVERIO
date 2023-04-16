a488da487562d6c46d9697e7b430f719b4f456ca
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-04-16 15:33:14 +0530

    Update Scroll.js

--- test/spec/Scroll.js
+++ test/spec/Scroll.js
@@ -1,8 +1,8 @@
 describe('Functional Testin in WebdriverIO',function(){
     it('Scrolling And MouseOver',async function (){
 
-        //https://only-testing-blog.blogspot.com/
-        //https://rahulshettyacademy.com/AutomationPractice/
+        ////https://only-testing-blog.blogspot.com/
+        ////https://rahulshettyacademy.com/AutomationPractice/
         await browser.url('https://rahulshettyacademy.com/AutomationPractice/');
         await browser.pause(3000);
         await (await $("#mousehover")).scrollIntoView();
