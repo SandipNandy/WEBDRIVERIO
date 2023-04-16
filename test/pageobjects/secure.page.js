290b616ef251afda15cf6eb2c6bd7cdb112292eb
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-04-16 15:32:12 +0530

    Update secure.page.js

--- test/pageobjects/secure.page.js
+++ test/pageobjects/secure.page.js
@@ -6,6 +6,7 @@ import Page from './page';
  ** sub page containing specific selectors and methods for a specific page
  */
 class SecurePage extends Page {
+    
     /**
      * define selectors using getter methods
      */
