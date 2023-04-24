2f840e3d0b71ebd00d5186e80a886557bad14fc0
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-04-24 08:18:56 +0530

    Update example.e2e.js

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -10,6 +10,7 @@ describe('My Login application', () => {
         await expect(SecurePage.flashAlert).toBeExisting();
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!');
+        
     });
 });
 
