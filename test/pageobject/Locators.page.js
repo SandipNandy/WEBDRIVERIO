2feba6e4114c92cdfcc3cdc1ee5cdb02668364e3
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-05-25 09:32:29 +0530

    Update Locators.page.js

--- test/pageobject/Locators.page.js
+++ test/pageobject/Locators.page.js
@@ -4,11 +4,9 @@ class LoginPage extends Page {
     get inputUsername() {
         return $('#username');
     }
-
     get inputPassword() {
         return $('#password');
     }
-
     get btnSubmit() {
         return $('button[type="submit"]');
     }
