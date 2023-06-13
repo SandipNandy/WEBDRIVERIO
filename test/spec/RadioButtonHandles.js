809faa77444ff65779f8a91f82941b8aa2172843
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-06-13 09:06:48 +0530

    Update RadioButtonHandles.js

--- test/spec/RadioButtonHandles.js
+++ test/spec/RadioButtonHandles.js
@@ -1,5 +1,4 @@
 describe('Radio Buttons Drop Down : ', () => {
-
     it('DEMO PAGE.', async () => {
         ////https://rahulshettyacademy.com/loginpagePractise/
         await browser.url('https://rahulshettyacademy.com/loginpagePractise/');
