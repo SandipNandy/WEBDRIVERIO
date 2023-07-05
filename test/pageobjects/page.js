cc3c7d9fa4da0b5b6fda2bf74317ed29c0de3ba3
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-05 09:29:08 +0530

    Update page.js

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -2,6 +2,7 @@
 * main page object containing all methods, selectors and functionality that is shared across all page objects that is shared across all page objects
 ** that is shared across all page objects
 */
+
 export default class Page {
     /**
     * Opens a sub page of the page
