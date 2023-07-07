48b978ec642c4b33647ada771c060f2295daf78c
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-07 14:46:06 +0530

    Update conf.js

--- conf.js
+++ conf.js
@@ -5,17 +5,13 @@ exports.config = {
   }],
   // Framework to use. Jasmine is recommended.
   framework: 'jasmine',
-
   // Spec patterns are relative to the current working directory when
   // protractor is called.
-
-  
   //specs: ['example_spec.js'],
   specs: [
     
     './test/specs/Main.js'
 ],
-
   jasmineOpts: {
     defaultTimeoutInterval: 30000
   }
