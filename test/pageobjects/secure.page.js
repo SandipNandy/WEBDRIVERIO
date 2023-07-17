1ff0bc2456cf7689eb5ab7167dc005b855d09fbf
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-17 09:21:49 +0530

    Update secure.page.js

--- test/pageobjects/secure.page.js
+++ test/pageobjects/secure.page.js
@@ -1,16 +1,15 @@
 import Page from './page';
+
 /**
  ** sub page containing specific selectors and methods for a specific page
  */
 class SecurePage extends Page {
-    
     /**
      * define selectors using getter methods
      */
     /**
      * define selectors using getter methods
-     */
-    
+     */ 
   get flashAlert() {
         return $('#flash');
     }
