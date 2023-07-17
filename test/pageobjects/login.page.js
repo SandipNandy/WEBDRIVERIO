d27815de5d8ac2c43563834d7c19600b6cc75a21
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-17 09:21:04 +0530

    Update login.page.js

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -2,7 +2,9 @@ import Page from './page';
 /**
  ** sub page containing specific selectors and methods for a specific page
  */
+
 class LoginPage extends Page {
+    
     /**
      ** define selectors using getter methods
      */
