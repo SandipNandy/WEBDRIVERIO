ef85156bcde8f24f416c8b60e4bdcc7043d1804e
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-17 09:24:53 +0530

    Update RadioButtonHandles.js

--- test/spec/RadioButtonHandles.js
+++ test/spec/RadioButtonHandles.js
@@ -1,12 +1,15 @@
 describe('Radio Buttons Drop Down : ', () => {
     it('DEMO PAGE.', async () => {
+        
         ////https://rahulshettyacademy.com/loginpagePractise/
         await browser.url('https://rahulshettyacademy.com/loginpagePractise/');
         await browser.maximizeWindow();
+        
         await $("[id='username']").setValue('rahulshettyacademy');
         await $("[id='password']").setValue("learning");
         const radioButton= await $$(".radiotextsty");
         let UserDropDown=radioButton[1];
+        
         await UserDropDown.$("span").click();
         await browser.pause(4000);
         //await (await $("[id='signInBtn']")).click();
