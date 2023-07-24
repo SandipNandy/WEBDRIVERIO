7a221081a13f1e9059b0ef2058c52c43b2f41202
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-24 16:51:24 +0530

    Update HandlingPopUpWIDO.js

--- test/spec/HandlingPopUpWIDO.js
+++ test/spec/HandlingPopUpWIDO.js
@@ -1,8 +1,11 @@
 describe('My Login application', () => {
+    
     it('should login with valid credentials', async() => {
+        
        await browser.url('https://rahulshettyacademy.com/loginpagePractise/');
         
        await $("#username").setValue("rahulshettyacademy");
+        
         await $('#password').setValue("learning");
         var radioButton = await $$('.customradio');
         
