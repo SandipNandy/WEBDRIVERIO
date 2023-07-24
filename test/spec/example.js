0389d6382a1dc4087c78edcf510cd28ddd89ad98
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-24 16:53:43 +0530

    Update example.js

--- test/spec/example.js
+++ test/spec/example.js
@@ -1,9 +1,13 @@
 ////const protractor=require('protractor');
 ////const driver1=browser;
 const assert = require('assert');
+
 import LoginPage from '../pageobject/Locators.page';
+
 describe('First Test', function () {
+    
     it('Main Scripts',async function () {
+        
         await LoginPage.open();
         await LoginPage.Title().then(function (tt) {
              console.log('Title : ', tt);
