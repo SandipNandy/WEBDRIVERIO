802e54059db8b09e16632f46eadf094a995a311a
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-08-04 11:39:50 +0530

    Update HappyPathswithAssertions.js

--- HappyPathswithAssertions.js
+++ HappyPathswithAssertions.js
@@ -3,6 +3,7 @@ describe('Assertions in Web DriverIO', () => {
     it('First Drop down', async () => {
         
         //https://rahulshettyacademy.com/loginpagePractise/
+        
         await browser.url('https://rahulshettyacademy.com/loginpagePractise/');
         await browser.maximizeWindow();
         await $("[id='username']").setValue('rahulshettyacademy');
