3642710dfef1b4d7b23fdc77704408f6c8756a85
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-08-14 09:57:59 +0530

    Update ArrayLogicWebdriverIO.js

--- test/spec/ArrayLogicWebdriverIO.js
+++ test/spec/ArrayLogicWebdriverIO.js
@@ -3,7 +3,6 @@ describe('My Login application', () => {
         //https://rahulshettyacademy.com/loginpagePractise/
        await browser.url('https://rahulshettyacademy.com/loginpagePractise/');
        await $("#username").setValue("rahulshettyacademy");
-        
         await $('#password').setValue("learning");
         var radioButton = await $$('.customradio');
        await radioButton[1].$("input[id='usertype']").click();
