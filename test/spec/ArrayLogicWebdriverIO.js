cd744350cb044ad23a12a5f1d2f17a6a89e4e02b
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-10 09:57:13 +0530

    Update ArrayLogicWebdriverIO.js

--- test/spec/ArrayLogicWebdriverIO.js
+++ test/spec/ArrayLogicWebdriverIO.js
@@ -10,6 +10,7 @@ describe('My Login application', () => {
         var radioButton = await $$('.customradio');
         
        await radioButton[1].$("input[id='usertype']").click();
+        
        await browser.pause(4000); 
     })
 });
