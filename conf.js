327b30e99f9b115bf6d94b30f7df890b89548a9e
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-10-01 09:45:15 +0530

    Update conf.js

--- conf.js
+++ conf.js
@@ -6,7 +6,6 @@ exports.config = {
   }],
   // Framework to use. Jasmine is recommended.
   framework: 'jasmine',
-  
   // Spec patterns are relative to the current working directory when
   // protractor is called.
   //specs: ['example_spec.js'],
