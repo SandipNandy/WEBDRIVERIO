75898893aa8c6855f28185b17e93fd9936f5bdf1
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-11-06 09:09:42 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -19,7 +19,7 @@ exports.config = {
     // If you are calling `wdio` from an NPM script (see https://docs.npmjs.com/cli/run-script),
     // then the current working directory is where your `package.json` resides, so `wdio`
     // will be called from there.
-    //
+    
     specs: [
         './test/spec/*.js'
     ],
