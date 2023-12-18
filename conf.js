92d737d4f8657de76bf0ab615590da6ce092ae16
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-12-18 09:04:09 +0530

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 exports.config = {
   capabilities: [{
     'browserName': 'chrome',
-    'browserName': 'chrome'
+    //'browserName': 'chrome'
   }],
   // Framework to use. Jasmine is recommended.
   framework: 'jasmine',
