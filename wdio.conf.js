38bf47f95fcacab0532fc0efb119a26fe48ab871
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-12-18 09:00:43 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -43,6 +43,7 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
+    
     maxInstances: 10,
     //
     // If you have trouble getting all important capabilities together, check out the
