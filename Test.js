1e63ace0ccdf377a0b909cea9acff7d8d2275ba6
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-12-18 09:05:51 +0530

    Update Test.js

--- Test.js
+++ Test.js
@@ -4,6 +4,7 @@ it('Launch URLS', function () {
    // browser.url('https://the-internet.herokuapp.com/login')
 });
 xit('refresh', function () {  
+    
     browser.refresh(); 
 })
 xit('u', function () {  
