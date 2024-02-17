f900f14108358a4b2a30347fa37318093d422077
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-02-17 09:09:38 +0530

    Update HandlingPopUpWIDO.js

--- test/spec/HandlingPopUpWIDO.js
+++ test/spec/HandlingPopUpWIDO.js
@@ -11,6 +11,7 @@ describe('My Login application', () => {
         var radioButton = await $$('.customradio');
         
        await radioButton[1].$("input[id='usertype']").click();
+        
       const popup=$(".modal-content")
        await popup.waitForDisplayed();
         (await $("button[id='cancelBtn']")).click();
