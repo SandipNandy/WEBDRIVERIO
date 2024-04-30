f631ee4caeccff2fd7c7d833572d97784f50f2f1
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-05-01 00:31:47 +0530

    Update Test.js

--- Test.js
+++ Test.js
@@ -10,6 +10,7 @@ xit('u', function () {
     //var t =
     browser.getTitle().then(function (tt) {
         console.log('Title : ', tt);
+        
         assert.equal(tt, 'The Internet');
     });
 });
