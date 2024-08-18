3907170e41545ab124d6748f85ec858bd762f8e7
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-08-18 14:45:51 +0530

    Update example.e2e.js

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -8,6 +8,7 @@ describe('My Login application', () => {
         
         //await LoginPage.open();
         await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        
         await expect(SecurePage.flashAlert).toBeExisting();
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!');
