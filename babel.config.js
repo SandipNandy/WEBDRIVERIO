f53086f18e69e5c0f5cd7f16afe7b12bd896711e
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-09-14 08:18:11 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -7,9 +7,7 @@ module.exports = {
             "@babel/preset-env",
             
             {
-                
                 "targets": {
-                    
                     "node": "14"   
                     
                 }
