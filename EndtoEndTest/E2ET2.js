fbe36acb932993d92ab608f72e10ec388d5bced3
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-09-19 10:05:15 +0530

    Update E2ET2.js

--- EndtoEndTest/E2ET2.js
+++ EndtoEndTest/E2ET2.js
@@ -14,6 +14,7 @@ describe('END TO END :-', () => {
         
         //wait until checkout button is displayed
         const link=$("*=Checkout");
+        
         await link.waitForExist();
         const cards= await $$("div[class='card h-100']");
         
