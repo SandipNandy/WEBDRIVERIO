7cb342b3b4767fdb8716816168a67873735911db
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-09-19 10:05:56 +0530

    Update example.e2e.js

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -3,6 +3,7 @@ import LoginPage from  '../pageobjects/login.page';
 import SecurePage from '../pageobjects/secure.page';
 
 describe('My Login application', () => {
+    
     it('should login with valid credentials', async () => {
         await LoginPage.open();
         
