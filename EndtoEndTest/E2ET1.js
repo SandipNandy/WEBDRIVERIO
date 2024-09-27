2e9fc622f893c18460e010b11453b34dcb854898
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-09-27 23:37:19 +0530

    Update E2ET1.js

--- EndtoEndTest/E2ET1.js
+++ EndtoEndTest/E2ET1.js
@@ -10,7 +10,6 @@ describe('END TO END :-', () => {
         const link=$("*=Checkout");
         await link.waitForExist();
         const cards= await $$("div[class='card h-100']");
-        
         for(let i=0;i<await cards.length;i++){
             
             const card= await cards[i].$("div h4 a");
