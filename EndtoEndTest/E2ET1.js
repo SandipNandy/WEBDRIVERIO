9cf519f2b2c4d05408f4dfe9d33d019347a9d492
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-09-27 23:36:43 +0530

    Update E2ET1.js

--- EndtoEndTest/E2ET1.js
+++ EndtoEndTest/E2ET1.js
@@ -7,7 +7,6 @@ describe('END TO END :-', () => {
         await $('#password').setValue("learning");
         await (await $("#signInBtn")).click();
         //wait until checkout button is displayed
-        
         const link=$("*=Checkout");
         
         await link.waitForExist();
