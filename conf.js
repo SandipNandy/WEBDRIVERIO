9b95307dccb39efe8d2103b2cb48d49736f18e05
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-04-16 01:20:03 +0530

    Update conf.js

--- conf.js
+++ conf.js
@@ -6,6 +6,7 @@ exports.config = {
     //'browserName': 'chrome'
   }],
   // Framework to use. Jasmine is recommended.
+  
   framework: 'jasmine',
   // Spec patterns are relative to the current working directory when
   // protractor is called.
