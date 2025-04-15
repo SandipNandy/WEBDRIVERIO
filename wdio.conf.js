38c48cf359ebf276b7b123f6fc336a59bb0a390f
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-04-16 01:16:12 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -8,7 +8,6 @@ exports.config = {
     // ==================
     // Define which test specs should run. The pattern is relative to the directory from which `wdio` was called.
     // The specs are defined as an array of spec files (optionally using wildcards
-    
     // that will be expanded). The test for each spec file will be run in a separate
 
     
