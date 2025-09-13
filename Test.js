0e5ba379bd24485c72ecbb179973338bb86a5890
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-09-13 10:58:15 +0530

    Update Test.js

--- Test.js
+++ Test.js
@@ -13,9 +13,11 @@ xit('u', function () {   //var t =
     browser.getTitle().then(function (tt) {
         
         console.log('Title : ', tt);
+        
         assert.equal(tt, 'The Internet');
     });
 });
+
 
 
 
